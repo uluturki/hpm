4b5045ae4c28971b39c06ad2d378bdc3589a733a
uluturki <uluturki@mail.usf.edu>
2018-09-23 22:49:40 -0400

    working function. still have space for improvement on optimization end.

--- hpm.py
+++ hpm.py
@@ -4,26 +4,30 @@ import random as rnd
 import itertools
 
 
-def generate_network(N, e_k, omega=2.5, random_seed = 42):
+# @DONE Automated tuning of c for desired e_k.
+# Caveat Emptor: This implementation doesn't worry too much about memory. random.random() is used,
+# take necessary precautions if you want results to be reproducible.
+def generate_network(N, e_k, omega=2.5):
     G = nx.empty_graph(N)
-
-    # First generate the hidden parameters
-    # For scale-free network, generate them using eq. 4.28
 
     # According to equation 4.29,  omega = (1 + 1/a)
     alpha = 1 / (omega - 1)
-    # Expected degree of resulting network can be controlled by expected value of
-    # hidden parameters n according to eq.s 4.27 and 4.28
 
-    # @TODO Automated tuning of c for desired e_k.
-    c = 114  # Constant c = 122.2 for N = 500 c = 684 for N = 5000, for a = 0.8
-    n_list = [c / np.power(i, alpha) for i in np.arange(1, N + 1)]
-    # Check if expected value is indeed what we are looking for.
-    n_expected = sum(n_list) / N
+    # @TODO Can also solve the series sum analytically, approximation by definite integrals for example.
+    # https://math.stackexchange.com/questions/1576502/calculate-finite-p-series
+
+    # Series = SUM 1/n^(alpha) FOR n FROM 1 to N
+    harmonic_series = np.power((1 / (np.arange(N) + 1)), alpha)
+    c = (e_k * N) / np.sum(harmonic_series)
+
+    # No need to recalculate the list of n, already have the harmonic series, just multiply by c, Eq. 4.28
+    n_list = harmonic_series * c
+
+    # n_expected is calculated to be e_k so it is unnecessary to recalculate it, to a very high precision.
+    # unless you want to verify that e_k is correct.
+    n_expected = e_k
 
     # Assign each node a hidden parameter n_i, they are generated above
-    # Nodes are indexed from 0 - N-1 by Networkx, we can just have these indexes
-    # be hidden parameter indexes and not bother with further assignments.
     edges = itertools.combinations(range(N), 2)
     for e in edges:
         # compute probability of connecting two nodes based on equation under
@@ -31,9 +35,5 @@ def generate_network(N, e_k, omega=2.5, random_seed = 42):
         p = ((n_list[e[0]] * n_list[e[1]]) / (n_expected * N))
         if rnd.random() < p:
             G.add_edge(*e)
-
-    #degrees = G.degree()
-    #sum_of_edges = sum(degrees.values())
-    #print(sum_of_edges / N)
 
     return G
